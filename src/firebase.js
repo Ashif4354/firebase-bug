1dc0cf8e9d0865db43765c9a76c8805a6f22c7dc
Ashif4354 <ashif.abhas@gmail.com>
2024-04-11 11:25:27 +0530

    removed firestore line

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
@@ -12,7 +11,5 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_measurementId
 }
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export default app;
-export { db };
